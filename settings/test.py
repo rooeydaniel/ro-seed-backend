e6346fb073090219d414843cec9edb7780c018ca
Daniel Stephenson <dan.stephenson@gmail.com>
2014-02-20 09:54:25 -0700

    Added secret key and CORS_WHITELIST

--- settings/test.py
+++ settings/test.py
@@ -1,6 +1,14 @@
 DEBUG = False
 
 # This should be set to the test domain (e.g. test.django-angular-pt.com)
-ALLOWED_HOSTS = ['quiet-basin-1853.herokuapp.com', 'daniel-angular.com']
+ALLOWED_HOSTS = ['cc-ro-seed-frontend.herokuapp.com', 'daniel-angular.com']
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '4444444444444444444'
+
+CORS_ORIGIN_WHITELIST = (
+    'cc-ro-seed-frontend.herokuapp.com',
+    'localhost/',
+)
 
 from base import *
