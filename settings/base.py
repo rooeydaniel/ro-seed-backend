0fa2d226d1dd8876d6983880824ae95b98a7c1a7
Daniel Stephenson <dan.stephenson@gmail.com>
2014-02-20 11:11:23 -0700

    Changing where wsgi.py points for its settings.

--- settings/base.py
+++ settings/base.py
@@ -39,18 +39,11 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'ro-seed',
-#         'USER': 'postgres',
-#         'PASSWORD': '',
-#         'HOST': 'cc-ro-seed-backend.herokuapp.com'
-#     }
-# }
-
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
