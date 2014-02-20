f2edeee2c811f89e856eb68afb995ec2e3a4ace4
Daniel Stephenson <dan.stephenson@gmail.com>
2014-02-20 11:00:43 -0700

    Changing where wsgi.py points for its settings.

--- settings/base.py
+++ settings/base.py
@@ -41,13 +41,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'project.db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ro-seed',
+        'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': 'cc-ro-seed-backend.herokuapp.com'
     }
 }
 
